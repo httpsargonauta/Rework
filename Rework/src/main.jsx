74e59ee0c153b532cadf54e44e3aacdf66d4c4ac
scarlhs26 <lycrek@gmail.com>
2024-03-16 16:37:47 -0400

    rework

--- Rework/src/main.jsx
+++ Rework/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import App from './App.jsx'
+//import home from './App.jsx'
 import './index.css'
 import { Home } from './pages/Home.jsx'
 
