333c56d84c741f34d5382a1d03312de8fd27cd5d
scarlhs26 <lycrek@gmail.com>
2024-03-16 14:28:15 -0400

    home

--- Rework/src/main.jsx
+++ Rework/src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App.jsx'
 import './index.css'
+import { Home } from './pages/Home.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+
+    </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
