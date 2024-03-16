cd7db32abff48c300adec468d8fb8de8dc337b96
httpsargonauta <soraadev@outlook.com>
2024-03-16 12:34:14 -0400

    gggg

--- Rework/src/App.jsx
+++ Rework/src/App.jsx
@@ -1,7 +1,11 @@
+import { Card } from "./components/card"
+
 export const Home = () => {
   return (
     <div>
       <h1>Home</h1>
+
+      <Card title="Filtrado inicial por IA" image="src/assets/Home/SVG/IA.svg" description="Utilizamos herramientas de inteligencia artificial para filtrar las propuestas de trabajo mas relevantes para usted segun los criterios de búsqueda establecidos." />
     </div>
   )
 }
--- Rework/src/components/card.jsx
+++ Rework/src/components/card.jsx
@@ -1,9 +1,9 @@
-export const Card = ({ image = '', title = '', description = '' }) => {
+export const Card = (props) => {
   return (
-    <div className="max-w-md flex flex-col gap-4 px-6 py-3">
-      <img className="w-32" src={image} alt={title} />
-      <h2 className="text-2xl font-bold">{title}</h2>
-      <p className="text-lg">{description}</p>  
+    <div className="max-w-md flex flex-col items-center bg-blue-600 rounded-xl shadow-lg gap-4 px-6 py-3">
+      <img className="w-32" src={props.image} alt={props.title} loading="lazy" decoding="async" />
+      <h2 className="text-2xl font-bold">{props.title}</h2>
+      <p className="text-lg">{props.description}</p>  
     </div>
   )
 }
