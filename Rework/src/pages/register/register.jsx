ad202b7c6e24937370f5c6d67e9e2ed1268d97d3
Eduardo <eduardokunmixyeah@gmail.com>
2024-03-16 20:59:43 -0400

    update register/page

--- Rework/src/pages/register/register.jsx
+++ Rework/src/pages/register/register.jsx
@@ -1,8 +1,16 @@
-import { CardEmply } from "../../components/cardEmply/CardEmply";
-import woman from '../../assets/Registro-empresas/Imagen de mujer - icon.png'
-import employe from '../../assets/Registro-empleados/Recurso-registro-empleado.png'
+import { useState } from "react";
+import { CardEmply } from "../components/CardEmply";
 
 export const Register = () => {
+  const [type, setType] = useState("password");
+
+  const showPassword = () => {
+    if (type === "password") {
+      setType("text");
+    } else {
+      setType("password");
+    }
+  };
   return (
     <main
       className="flex flex-col items-center w-full gap-4 h-screen bg-cover"
@@ -31,7 +39,7 @@ export const Register = () => {
           <CardEmply>
             <div className="flex flex-col items-center w-[142px] h-[140px]">
               <img
-                src={employe}
+                src="src/assets/Registro-1/Recurso-registro  (2).svg"
                 className="size-28"
                 alt="Remote Employed"
               />
@@ -43,7 +51,7 @@ export const Register = () => {
           <CardEmply>
             <div className="flex flex-col items-center w-[142px] h-[140px]">
               <img
-                src={woman}
+                src="src/assets/Registro-1/Recurso-registro  (2).svg"
                 className="size-28"
                 alt="Remote Employed"
               />
@@ -61,8 +69,43 @@ export const Register = () => {
           action=""
           onSubmit={(e) => e.preventDefault()}
         >
-          <input type="text" placeholder="email" />
-          <input type="text" placeholder="Contraseña" />
+          <div className="flex items-center justify-start gap-2 bg-[#DBDBDB] px-4 py-2 rounded-lg">
+            <label htmlFor="email">
+              <img
+                src="src/assets/Registro-1/Recurso-registro  (4).svg"
+                alt=""
+                className="size-8"
+              />
+            </label>
+            <input
+              type="email"
+              placeholder="User or Email"
+              className="bg-transparent focus:outline-none text-md font-semibold text-tertiary text-start items-center justify-center"
+            />
+          </div>
+
+          <div className="flex items-center justify-start gap-2 bg-[#DBDBDB] px-4 py-2 rounded-lg">
+            <label htmlFor="email">
+              <img
+                src="src/assets/Registro-1/Recurso-registro  (4).svg"
+                alt=""
+                className="size-8"
+              />
+            </label>
+            <input
+              type={type}
+              placeholder="User or Email"
+              className="bg-transparent focus:outline-none text-md font-semibold text-tertiary text-start items-center justify-center flex-1"
+            />
+            <label htmlFor="email" onClick={showPassword}>
+              <img
+                src="src/assets/Registro-1/Recurso-registro  (4).svg"
+                alt=""
+                className="size-8"
+              />
+            </label>
+          </div>
+
           <button type="submit">Inicia sesion</button>
         </form>
       </section>
