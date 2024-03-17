becf3399cb2448206d371947c5c0e91649a0389b
Eduardo <eduardokunmixyeah@gmail.com>
2024-03-16 20:56:50 -0400

    change import img

---
+++ Rework/src/pages/register/register.jsx
@@ -0,0 +1,71 @@
+import { CardEmply } from "../../components/cardEmply/CardEmply";
+import woman from '../../assets/Registro-empresas/Imagen de mujer - icon.png'
+import employe from '../../assets/Registro-empleados/Recurso-registro-empleado.png'
+
+export const Register = () => {
+  return (
+    <main
+      className="flex flex-col items-center w-full gap-4 h-screen bg-cover"
+      style={{
+        backgroundImage:
+          "url('src/assets/Registro-1/Recurso-registro  (1).png')",
+      }}
+    >
+      <section className="flex flex-col items-center justify-center gap-3 mt-12">
+        <img
+          src="src/assets/Registro-1/Recurso-registro  (3).png"
+          className="size-48"
+          alt="Remote Employed"
+        />
+        <h2 className="text-5xl font-semibold">Bienvenido!</h2>
+        <h2 className="bg-secondary px-2 py-1.5 font-semibold text-3xl text-white rounded-md">
+          Profesionales en Remoto
+        </h2>
+      </section>
+
+      <section className="mt-8 flex flex-col items-center gap-4">
+        <h3 className="text-2xl font-semibold text-tertiary">
+          Que estas buscando?
+        </h3>
+        <div className="flex gap-4">
+          <CardEmply>
+            <div className="flex flex-col items-center w-[142px] h-[140px]">
+              <img
+                src={employe}
+                className="size-28"
+                alt="Remote Employed"
+              />
+              <h3 className="text-xl font-medium  text-tertiary mt-2">
+                Empleo Remoto
+              </h3>
+            </div>
+          </CardEmply>
+          <CardEmply>
+            <div className="flex flex-col items-center w-[142px] h-[140px]">
+              <img
+                src={woman}
+                className="size-28"
+                alt="Remote Employed"
+              />
+              <h3 className="text-xl font-medium  text-tertiary mt-2">
+                Trabajadores
+              </h3>
+            </div>
+          </CardEmply>
+        </div>
+      </section>
+
+      <section className="mt-12 flex flex-col items-center gap-4">
+        <form
+          className="flex flex-col gap-4"
+          action=""
+          onSubmit={(e) => e.preventDefault()}
+        >
+          <input type="text" placeholder="email" />
+          <input type="text" placeholder="Contraseña" />
+          <button type="submit">Inicia sesion</button>
+        </form>
+      </section>
+    </main>
+  );
+};
